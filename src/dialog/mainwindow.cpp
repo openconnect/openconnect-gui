2c6b82f8132a8554e2cd40f90cbea476df0b2731
Ľubomír Carik <Lubomir.Carik@gmail.com>
2017-01-15 23:29:04 +0100

    Add links to 3rd party libraries on About dialog

--- src/dialog/mainwindow.cpp
+++ src/dialog/mainwindow.cpp
@@ -844,12 +844,12 @@ void MainWindow::on_actionRemoveSelectedProfile_triggered()
 void MainWindow::on_actionAbout_triggered()
 {
     QString txt = QLatin1String("<h2>") + QLatin1String(appDescriptionLong) + QLatin1String("</h2>");
-    txt += tr("Version: ") + QLatin1String("<i>") + QLatin1String(appVersion) + QLatin1String("</i>");
-    txt += tr("<br><br>Based on:");
-    txt += tr("<br>- libopenconnect ") + QLatin1String(openconnect_get_version());
-    txt += tr("<br>- GnuTLS ") + QLatin1String(gnutls_check_version(nullptr));
-    txt += tr("<br>- Qt %1 (%2 bit)").arg(QT_VERSION_STR).arg(QSysInfo::buildCpuArchitecture() == "i386" ? 32 : 64);
+    txt += tr("Version ") + QLatin1String("<i>") + QLatin1String(appVersion) + QLatin1String("</i>");
     txt += tr("<br><br>Build on ") + QLatin1String("<i>") + QLatin1String(appBuildOn) + QLatin1String("</i>");
+    txt += tr("<br>Based on");
+    txt += tr("<br>- <a href=\"https://www.infradead.org/openconnect\">OpenConnect</a> ") + QLatin1String(openconnect_get_version());
+    txt += tr("<br>- <a href=\"https://www.gnutls.org\">GnuTLS</a> v") + QLatin1String(gnutls_check_version(nullptr));
+    txt += tr("<br>- <a href=\"https://www.qt.io\">Qt</a> v%1 (%2 bit)").arg(QT_VERSION_STR).arg(QSysInfo::buildCpuArchitecture() == "i386" ? 32 : 64);
     txt += tr("<br><br>%1<br>").arg(appCopyright);
     txt += tr("<br><i>%1</i> comes with ABSOLUTELY NO WARRANTY. This is free software, "
               "and you are welcome to redistribute it under the conditions "
