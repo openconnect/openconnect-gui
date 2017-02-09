5ea5f385f578d0a9ecdf97bb412e2befd97ae076
Ľubomír Carik <Lubomir.Carik@gmail.com>
2017-02-09 21:41:58 +0100

    CMake re-indent
    Signed-off-by: Ľubomír Carik <Lubomir.Carik@gmail.com>

--- CMake/Includes/git_revision_generate.cmake
+++ CMake/Includes/git_revision_generate.cmake
@@ -3,53 +3,53 @@
 #create a tag with 'git tag <name>' and 'git push --tags'
 
 if(IS_DIRECTORY ${GIT_ROOT_DIR}/.git)
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
-		WORKING_DIRECTORY ${GIT_ROOT_DIR}
-		RESULT_VARIABLE res_var
-		OUTPUT_VARIABLE GIT_COM_ID
-	)
-	if(NOT ${res_var} EQUAL 0)
-		set(GIT_COMMIT_ID "?.?.?-unknown")
-		message(WARNING "Git failed (not a repo, or no tags). Build will not contain git revision info.")
-	endif()
-	string(REGEX REPLACE "\n$" "" GIT_COMMIT_ID ${GIT_COM_ID})
-	string(REGEX REPLACE "^v" "" GIT_COMMIT_ID ${GIT_COMMIT_ID})
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
+        WORKING_DIRECTORY ${GIT_ROOT_DIR}
+        RESULT_VARIABLE res_var
+        OUTPUT_VARIABLE GIT_COM_ID
+    )
+    if(NOT ${res_var} EQUAL 0)
+        set(GIT_COMMIT_ID "?.?.?-unknown")
+        message(WARNING "Git failed (not a repo, or no tags). Build will not contain git revision info.")
+    endif()
+    string(REGEX REPLACE "\n$" "" GIT_COMMIT_ID ${GIT_COM_ID})
+    string(REGEX REPLACE "^v" "" GIT_COMMIT_ID ${GIT_COMMIT_ID})
 
-	# check number of digits in version string
-	string(REPLACE "." ";" GIT_COMMIT_ID_VLIST ${GIT_COMMIT_ID})
-	list(LENGTH GIT_COMMIT_ID_VLIST GIT_COMMIT_ID_VLIST_COUNT)
+    # check number of digits in version string
+    string(REPLACE "." ";" GIT_COMMIT_ID_VLIST ${GIT_COMMIT_ID})
+    list(LENGTH GIT_COMMIT_ID_VLIST GIT_COMMIT_ID_VLIST_COUNT)
 
-	# no.: major
-	string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${GIT_COMMIT_ID}")
-	# no.: minor
-	string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_COMMIT_ID}")
+    # no.: major
+    string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${GIT_COMMIT_ID}")
+    # no.: minor
+    string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_COMMIT_ID}")
 
-	if(${GIT_COMMIT_ID_VLIST_COUNT} STREQUAL "2")
-		# no. patch
-		set(VERSION_PATCH "0")
-		# SHA1 string + git 'dirty' flag
-		string(REGEX REPLACE "^v[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${GIT_COMMIT_ID}")
-	else()
-		# no. patch
-		string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${GIT_COMMIT_ID}")
-		# SHA1 string + git 'dirty' flag
-		string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${GIT_COMMIT_ID}")
-	endif()
+    if(${GIT_COMMIT_ID_VLIST_COUNT} STREQUAL "2")
+        # no. patch
+        set(VERSION_PATCH "0")
+        # SHA1 string + git 'dirty' flag
+        string(REGEX REPLACE "^v[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${GIT_COMMIT_ID}")
+    else()
+        # no. patch
+        string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${GIT_COMMIT_ID}")
+        # SHA1 string + git 'dirty' flag
+        string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${GIT_COMMIT_ID}")
+    endif()
 
-	set(PROJECT_VERSION "${GIT_COMMIT_ID}")
-	#string(APPEND PROJECT_VERSION " (git)")
-	message(STATUS "Version: ${PROJECT_VERSION} [git]")
+    set(PROJECT_VERSION "${GIT_COMMIT_ID}")
+    #string(APPEND PROJECT_VERSION " (git)")
+    message(STATUS "Version: ${PROJECT_VERSION} [git]")
 else()
-	message(STATUS "Version: ${PROJECT_VERSION} [cmake]")
+    message(STATUS "Version: ${PROJECT_VERSION} [cmake]")
 endif()
 
 if(NOT APPLE)
-	if(PROJ_UAC_ON)
-		set(UAC_FLAG "")
-	else()
-		set(UAC_FLAG "//")
-	endif()
+    if(PROJ_UAC_ON)
+        set(UAC_FLAG "")
+    else()
+        set(UAC_FLAG "//")
+    endif()
 
     message(STATUS "Processing resource file...")
     file(READ ${INPUT_DIR}/${PROJECT_NAME}.rc.in rc_temporary)
@@ -66,6 +66,6 @@ file(READ ${INPUT_DIR}/${PROJECT_NAME}.cpp.in cpp_temporary)
 string(CONFIGURE "${cpp_temporary}" cpp_updated @ONLY)
 file(WRITE ${OUTPUT_DIR}/${PROJECT_NAME}.cpp.tmp "${cpp_updated}")
 execute_process(
-	COMMAND ${CMAKE_COMMAND} -E copy_if_different
-	${OUTPUT_DIR}/${PROJECT_NAME}.cpp.tmp ${OUTPUT_DIR}/${PROJECT_NAME}.cpp
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    ${OUTPUT_DIR}/${PROJECT_NAME}.cpp.tmp ${OUTPUT_DIR}/${PROJECT_NAME}.cpp
 )
