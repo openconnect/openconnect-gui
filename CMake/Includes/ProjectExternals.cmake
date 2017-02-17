fe50910f24c65fbc9b48094457f600051b9dc1db
Ľubomír Carik <Lubomir.Carik@gmail.com>
2017-02-17 01:22:31 +0100

    Download 3rd party artifacts via CMake instead of git-submodules.
    This should provide us better control about artifacts
    on different target platforms.
    
    Signed-off-by: Ľubomír Carik <Lubomir.Carik@gmail.com>

---
+++ CMake/Includes/ProjectExternals.cmake
@@ -0,0 +1,201 @@
+set(openconnect-TAG v7.08)
+set(vpnc-scripts-TAG master)
+#set(spdlog-TAG v0.11.0)
+set(spdlog-TAG master)
+set(qt-solutions-TAG master)
+
+
+include(ExternalProject)
+
+# --------------------------------------------------------------------------------------------------
+# openconnect (libs, headers, etc.)
+# --------------------------------------------------------------------------------------------------
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/external/openconnect-devel-${openconnect-TAG}_mingw32.zip)
+    set(OPENCONNECT_URL https://github.com/horar/openconnect/releases/download/${openconnect-TAG})
+else()
+    message(STATUS "Using local openconnect-devel packages...")
+    set(OPENCONNECT_URL ${CMAKE_SOURCE_DIR}/external)
+endif()
+
+ExternalProject_Add(openconnect-devel-${openconnect-TAG}
+    PREFIX ${CMAKE_BINARY_DIR}/external
+    INSTALL_DIR ${CMAKE_SOURCE_DIR}/external
+    
+    DOWNLOAD_NO_PROGRESS 1
+    
+    URL ${OPENCONNECT_URL}/openconnect-devel-${openconnect-TAG}_mingw32.zip
+
+    BUILD_COMMAND ""
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
+)
+ExternalProject_Add_Step(openconnect-devel-${openconnect-TAG} deploy_headers
+    COMMAND ${CMAKE_COMMAND} -E copy_directory include <INSTALL_DIR>/include
+    COMMENT "...deploing openconnect-${openconnect-TAG} headers"
+    WORKING_DIRECTORY <SOURCE_DIR>
+    DEPENDEES install
+)
+ExternalProject_Add_Step(openconnect-devel-${openconnect-TAG} deploy_libraries
+    COMMAND ${CMAKE_COMMAND} -E copy_directory lib <INSTALL_DIR>/lib
+    COMMENT "...deploing openconnect-${openconnect-TAG} libraries"
+    WORKING_DIRECTORY <SOURCE_DIR>
+    DEPENDEES install
+    ALWAYS 0
+)
+
+# --------------------------------------------------------------------------------------------------
+# openconnect (util & libs)
+# --------------------------------------------------------------------------------------------------
+ExternalProject_Add(openconnect-${openconnect-TAG}
+    PREFIX ${CMAKE_BINARY_DIR}/external
+    INSTALL_DIR ${CMAKE_SOURCE_DIR}/external
+    
+    URL ${OPENCONNECT_URL}/openconnect-${openconnect-TAG}_mingw32.zip
+
+    BUILD_COMMAND ""
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
+)
+ExternalProject_Add_Step(openconnect-${openconnect-TAG} deploy_libs
+    COMMAND ${CMAKE_COMMAND} -E copy_directory . <INSTALL_DIR>/lib
+    COMMENT "...deploing openconnect-${openconnect-TAG} libraries"
+    WORKING_DIRECTORY <SOURCE_DIR>
+    DEPENDEES install
+)
+
+add_library(libgmp SHARED IMPORTED)
+set_property(TARGET libgmp PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libgmp-10.dll)
+set_property(TARGET libgmp PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libgmp.dll.a)
+
+add_library(libgnutls SHARED IMPORTED)
+set_property(TARGET libgnutls PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libgnutls-30.dll)
+set_property(TARGET libgnutls PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libgnutls.dll.a)
+
+add_library(libhogweed SHARED IMPORTED)
+set_property(TARGET libhogweed PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libhogweed-4.dll)
+set_property(TARGET libhogweed PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libhogweed.dll.a)
+
+add_library(libnettle SHARED IMPORTED)
+set_property(TARGET libnettle PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libnettle-6.dll)
+set_property(TARGET libnettle PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libnettle.dll.a)
+
+add_library(libopenconnect SHARED IMPORTED)
+set_property(TARGET libopenconnect PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libopenconnect-5.dll)
+set_property(TARGET libopenconnect PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libopenconnect.dll.a)
+
+add_library(libp11-kit SHARED IMPORTED)
+set_property(TARGET libp11-kit PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libp11-kit-0.dll)
+set_property(TARGET libp11-kit PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libp11-kit.dll.a)
+
+add_library(libstoken SHARED IMPORTED)
+set_property(TARGET libstoken PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libstoken-1.dll)
+set_property(TARGET libstoken PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libstoken.dll.a)
+
+add_library(libxml2 SHARED IMPORTED)
+set_property(TARGET libxml2 PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/lib/libxml2-2.dll)
+set_property(TARGET libxml2 PROPERTY IMPORTED_IMPLIB ${CMAKE_SOURCE_DIR}/external/lib/libxml2.dll.a)
+
+
+# --------------------------------------------------------------------------------------------------
+# spdlog
+# --------------------------------------------------------------------------------------------------
+file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/external/include)
+ExternalProject_Add(spdlog-${spdlog-TAG}
+    PREFIX ${CMAKE_BINARY_DIR}/external
+    
+    INSTALL_DIR ${CMAKE_SOURCE_DIR}/external
+    
+    UPDATE_DISCONNECTED 0
+    UPDATE_COMMAND ""
+    
+    #DOWNLOAD_NO_PROGRESS 1
+    
+    GIT_REPOSITORY https://github.com/gabime/spdlog
+    GIT_TAG ${spdlog-TAG}
+    GIT_SHALLOW 1
+
+    # CMake generator
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DBUILD_TESTING=off -DSPDLOG_BUILD_TESTING=off
+)
+
+add_library(spdlog::spdlog INTERFACE IMPORTED)
+set_target_properties(spdlog::spdlog PROPERTIES
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_SOURCE_DIR}/external/include"
+)
+
+
+# --------------------------------------------------------------------------------------------------
+# qt-solutions
+# --------------------------------------------------------------------------------------------------
+ExternalProject_Add(qt-solutions-${qt-solutions-TAG}
+    PREFIX ${CMAKE_BINARY_DIR}/external/
+    #DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/download
+    #SOURCE_DIR ${CMAKE_BINARY_DIR}/external
+    
+    ##INSTALL_DIR ${CMAKE_BINARY_DIR}/external
+    ##CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DSPDLOG_BUILD_TESTING=off
+    
+    UPDATE_DISCONNECTED 0
+    UPDATE_COMMAND ""
+    
+    #DOWNLOAD_NO_PROGRESS 1
+    
+    #GIT_REPOSITORY https://github.com/qtproject/qt-solutions.git
+    GIT_REPOSITORY https://code.qt.io/qt-solutions/qt-solutions.git
+    GIT_TAG ${qt-solutions-TAG}
+    GIT_SHALLOW 1
+    
+    BUILD_IN_SOURCE 1
+    
+    #SOURCE_SUBDIR qtsingleapplication
+    #SOURCE_DIR ${CMAKE_BINARY_DIR}/external/src/qt-solutions/qtsingleapplication/qtsingleapplication
+    #CONFIGURE_COMMAND configure -library
+    #CONFIGURE_COMMAND qmake qtsingleapplication/qtsingleapplication.pro PREFIX=${CMAKE_SOURCE_DIR}/external
+    CONFIGURE_COMMAND ""#cd qtsingleapplication && configure.bat -library && qmake qtsingleapplication.pro
+   
+    BUILD_COMMAND ""#cd qtsingleapplication && mingw32-make
+    INSTALL_COMMAND ""#cd qtsingleapplication && mingw32-make install
+)
+
+
+
+
+
+# --------------------------------------------------------------------------------------------------
+# vpnc-scripts
+# --------------------------------------------------------------------------------------------------
+ExternalProject_Add(vpnc-scripts-${vpnc-scripts-TAG}
+    PREFIX ${CMAKE_BINARY_DIR}/external/
+    #DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/download
+    #SOURCE_DIR ${CMAKE_BINARY_DIR}/external
+    
+    ##INSTALL_DIR ${CMAKE_BINARY_DIR}/external
+    ##CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DSPDLOG_BUILD_TESTING=off
+    
+    UPDATE_DISCONNECTED 0
+    UPDATE_COMMAND ""
+    
+    #DOWNLOAD_NO_PROGRESS 1
+    
+    #GIT_REPOSITORY https://github.com/qtproject/qt-solutions.git
+    GIT_REPOSITORY git://git.infradead.org/users/dwmw2/vpnc-scripts.git
+    GIT_TAG ${vpnc-scripts-TAG}
+    GIT_SHALLOW 1
+    
+    BUILD_IN_SOURCE 1
+    
+    #SOURCE_SUBDIR qtsingleapplication
+    #SOURCE_DIR ${CMAKE_BINARY_DIR}/external/src/qt-solutions/qtsingleapplication/qtsingleapplication
+    #CONFIGURE_COMMAND configure -library
+    #CONFIGURE_COMMAND qmake qtsingleapplication/qtsingleapplication.pro PREFIX=${CMAKE_SOURCE_DIR}/external
+    CONFIGURE_COMMAND ""
+   
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different vpnc-script-win.js ${CMAKE_SOURCE_DIR}/external/vpnc-script.js
+)
+#ExternalProject_Add_Step(openconnect-devel-${openconnect-TAG} deploy_headers
+#    COMMAND ${CMAKE_COMMAND} -E copy_directory include <INSTALL_DIR>/include
+#    COMMENT "...deploing openconnect-${openconnect-TAG} headers"
+#    WORKING_DIRECTORY <SOURCE_DIR>
+#    DEPENDEES install
+#)
---
+++ CMake/Includes/ProjectPackage.cmake
@@ -0,0 +1,117 @@
+include(InstallRequiredSystemLibraries)
+
+# common CPacke variables
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+set(CPACK_PACKAGE_VENDOR ${PRODUCT_NAME_COMPANY})
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+#set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/README)
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY ${PRODUCT_NAME_LONG})
+#set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME})
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PRODUCT_NAME_SHORT}")
+if(APPLE)
+    set(CPACK_PACKAGE_ICON ${CMAKE_CURRENT_SOURCE_DIR}/src/images/mono_lock.icns)
+endif()
+set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt)
+#set(CPACK_RESOURCE_FILE_README ...)
+#set(CPACK_RESOURCE_FILE_WELCOME ...)
+if(WIN32 AND MINGW)
+    set(CPACK_GENERATOR "NSIS")
+
+    set(CPACK_NSIS_INSTALLED_ICON_NAME "${PROJECT_NAME}.exe")
+    set(CPACK_NSIS_DISPLAY_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY}")
+    set(CPACK_NSIS_HELP_LINK "https:\\\\\\\\github.com\\\\openconnect\\\\openconnect-gui\\\\wiki\\\\FAQ")
+    set(CPACK_NSIS_URL_INFO_ABOUT "https:\\\\\\\\openconnect.github.io\\\\openconnect-gui")
+    set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
+    set(CPACK_NSIS_MODIFY_PATH ON)
+    set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL OFF)
+
+    set(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}\\\\src\\\\${PROJECT_NAME}.ico")
+    set(CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_SOURCE_DIR}\\\\src\\\\${PROJECT_NAME}.ico")
+
+    set(CPACK_PACKAGE_ICON ${CMAKE_CURRENT_SOURCE_DIR}\\\\nsis\\\\images\\\\header-openconnect.bmp)
+    set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP ${CMAKE_CURRENT_SOURCE_DIR}\\\\nsis\\\\images\\\\install-openconnect.bmp)
+    set(CPACK_NSIS_MUI_UNWELCOMEFINISHPAGE_BITMAP ${CMAKE_CURRENT_SOURCE_DIR}\\\\nsis\\\\images\\\\install-openconnect.bmp)
+
+    set(CPACK_NSIS_EXECUTABLES_DIRECTORY ".")
+    set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${PROJECT_NAME}.exe")
+
+    set(CPACK_NSIS_MENU_LINKS
+        "https://openconnect.github.io/openconnect-gui/" "Homepage"
+        "https://github.com/openconnect/openconnect-gui/issues" "Issues"
+        "https://github.com/openconnect/openconnect-gui/wiki/FAQ" "FAQ"
+    )
+
+    # NSIS'es list of all components
+    set(CPACK_COMPONENTS_ALL App App_Console vpnc_script TAP_drivers VcRedist_libs)
+
+    set(CPACK_COMPONENT_APP_REQUIRED on)
+    set(CPACK_COMPONENT_APP_DISPLAY_NAME "${PRODUCT_NAME_SHORT}")
+    set(CPACK_COMPONENT_APP_DESCRIPTION "${PRODUCT_NAME_LONG} GUI")
+    set(CPACK_COMPONENT_APP_GROUP "Application")
+    set(CPACK_COMPONENT_APP_INSTALL_TYPES Full AppOnly Standard)
+
+    set(CPACK_COMPONENT_VPNC_SCRIPT_REQUIRED on)
+    set(CPACK_COMPONENT_VPNC_SCRIPT_DISPLAY_NAME "vpnc-script.js")
+    set(CPACK_COMPONENT_VPNC_SCRIPT_DESCRIPTION "vpnc helper script to set the routing and name service up")
+    set(CPACK_COMPONENT_VPNC_SCRIPT_GROUP "Application")
+    set(CPACK_COMPONENT_VPNC_SCRIPT_INSTALL_TYPES Full AppOnly Standard)
+
+    set(CPACK_COMPONENT_TAP_DRIVERS_REQUIRED off)
+    set(CPACK_COMPONENT_TAP_DRIVERS_DISPLAY_NAME "TAP driver")
+    set(CPACK_COMPONENT_TAP_DRIVERS_DESCRIPTION "NDIS 6 driver for Windows (in case you're installing on Windows XP, \
+    disable this option and install NDIS 5 drivers manually from https://openvpn.net)")
+    set(CPACK_COMPONENT_TAP_DRIVERS_GROUP "Drivers")
+    set(CPACK_COMPONENT_TAP_DRIVERS_INSTALL_TYPES Full Standard)
+
+    set(CPACK_COMPONENT_VCREDIST_LIBS_REQUIRED off)
+    set(CPACK_COMPONENT_VCREDIST_LIBS_DISPLAY_NAME "vcredist")
+    set(CPACK_COMPONENT_VCREDIST_LIBS_DESCRIPTION "Visual C++ Redistributable Package for Visual Studio 2015")
+    set(CPACK_COMPONENT_VCREDIST_LIBS_GROUP "Drivers")
+    set(CPACK_COMPONENT_VCREDIST_LIBS_INSTALL_TYPES Full Standard)
+
+    set(CPACK_COMPONENT_APP_CONSOLE_DISABLED on)
+    set(CPACK_COMPONENT_APP_CONSOLE_REQUIRED off)
+    set(CPACK_COMPONENT_APP_CONSOLE_DISPLAY_NAME "${PRODUCT_NAME_SHORT}")
+    set(CPACK_COMPONENT_APP_CONSOLE_DESCRIPTION "${PRODUCT_NAME_LONG} (console)")
+    set(CPACK_COMPONENT_APP_CONSOLE_GROUP "Application")
+    set(CPACK_COMPONENT_APP_CONSOLE_INSTALL_TYPES Full)
+
+    # custom install command to populate vcredist & NDIS drivers
+    # vcredis: http://asawicki.info/news_1597_installing_visual_c_redistributable_package_from_command_line.html
+    list(APPEND CPACK_NSIS_EXTRA_INSTALL_COMMANDS " ExecWait '\\\"$INSTDIR\\\\Drivers\\\\vcredist_x86.exe\\\" /install /quiet /norestart'")
+    list(APPEND CPACK_NSIS_EXTRA_INSTALL_COMMANDS " ExecWait '\\\"$INSTDIR\\\\Drivers\\\\tap-windows.exe\\\" /S /norestart'")
+    string(REPLACE ";" "\n" CPACK_NSIS_EXTRA_INSTALL_COMMANDS "${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}")
+
+    # NSIS'es Runtime-group
+    set(CPACK_COMPONENT_GROUP_APPLICATION_DESCRIPTION "Main application and network configuration script")
+    set(CPACK_COMPONENT_GROUP_APPLICATION_EXPANDED on)
+    set(CPACK_COMPONENT_GROUP_DRIVERS_DESCRIPTION "Drivers")
+    set(CPACK_COMPONENT_GROUP_DRIVERS_EXPANDED on)
+
+    # NSIS'es install types lists
+    set(CPACK_ALL_INSTALL_TYPES Full)
+    set(CPACK_INSTALL_TYPE_FULL_DISPLAY_NAME "Full installation")
+    set(CPACK_INSTALL_TYPE_APPONLY_DISPLAY_NAME "Application only")
+    set(CPACK_INSTALL_TYPE_STANDARD_DISPLAY_NAME "Standard installation")
+
+    # source code packaging
+    #set(CPACK_SOURCE_PACKAGE_FILE_NAME )
+    set(CPACK_SOURCE_GENERATOR "7Z")
+    set(CPACK_SOURCE_IGNORE_FILES "build-release/;/\.git/;\.swp$;\.gitignore$")
+else()
+    set(CPACK_GENERATOR "DragNDrop")
+
+    #set(CPACK_DMG_VOLUME_NAME ...)
+    #set(CPACK_DMG_FORMAT ...)
+    #set(CPACK_DMG_DS_STORE ...)
+    #set(CPACK_DMG_DS_STORE_SETUP_SCRIPT ...)
+    #set(CPACK_DMG_BACKGROUND_IMAGE ...)
+    #set(CPACK_DMG_SLA_DIR ...)
+    #set(CPACK_DMG_SLA_LANGUAGES ...)
+endif()
+set(CPACK_PACKAGE_EXECUTABLES "${PROJECT_NAME}" "${PRODUCT_NAME_LONG}")
+set(CPACK_CREATE_DESKTOP_LINKS  "${PROJECT_NAME}")
+
+include(CPack)
