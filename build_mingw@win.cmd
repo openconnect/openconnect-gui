9bc345e336c87ead50de2fed98fbc4fd39ad9099
Ľubomír Carik <Lubomir.Carik@gmail.com>
2018-01-15 00:54:45 +0100

    Update Qt version path in sample build script for Windows
    Signed-off-by: Ľubomír Carik <Lubomir.Carik@gmail.com>

--- build_mingw@win.cmd
+++ build_mingw@win.cmd
@@ -5,16 +5,16 @@ REM
 REM It should be used only as illustration how to build application
 REM and create an installer package
 REM
-REM (c) 2016-2017, Lubomir Carik
+REM (c) 2016-2018, Lubomir Carik
 REM
 
 echo "======================================================================="
 echo " Preparing environment..."
 echo "======================================================================="
-REM look for "Qt 5.9.1 for Desktop (MinGW 5.3.0 32 bit)" StartMenu item
+REM look for "Qt 5.10.0 for Desktop (MinGW 5.3.0 32 bit)" StartMenu item
 REM and check 'qtenv2.bat'
 echo Setting up environment for Qt usage...
-set PATH=C:\Dev\Qt\5.9.1\mingw53_32\bin\;%PATH%
+set PATH=C:\Dev\Qt\5.10.0\mingw53_32\bin\;%PATH%
 
 echo Setting up environment for 'mingw32' usage...
 set PATH=c:\Dev\Qt\Tools\mingw530_32\bin\;%PATH%
