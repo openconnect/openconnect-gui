6895e42cacc7455f106dc18a1cba9fbddb2dc8d1
Ľubomír Carik <Lubomir.Carik@gmail.com>
2018-02-19 02:19:18 +0100

    Sample mingw@msys build script

---
+++ build_msys2@win.sh
@@ -0,0 +1,50 @@
+#
+# Sample build script & release package preapration
+#
+# It should be used only as illustration how to build application
+# and create an installer package
+#
+# (c) 2016-2018, Lubomir Carik
+#
+
+echo "======================================================================="
+echo " Preparing sandbox..."
+echo "======================================================================="
+mkdir -pv build-release
+
+echo "======================================================================="
+echo " Generating project..."
+echo "======================================================================="
+cd build-release
+cmake -G "MSYS Makefiles" \
+	-DCMAKE_BUILD_TYPE=Release \
+	..
+
+echo "======================================================================="
+echo " Compiling..."
+echo "======================================================================="
+make -j5
+
+# echo "======================================================================="
+# echo " LC: Bundling... (dynamic Qt only)"
+# echo "======================================================================="
+# rd /s /q out
+# md out
+# windeployqt ^
+# 	src\openconnect-gui.exe ^
+# 	--verbose 1 ^
+# 	--compiler-runtime ^
+# 	--release ^
+# 	--force ^
+# 	--no-webkit2 ^
+# 	--no-quick-import ^
+# 	--no-translations
+
+echo "======================================================================="
+echo " Packaging..."
+echo "======================================================================="
+cmake .
+make package VERBOSE=1
+# make package_source VERBOSE=1
+
+cd ..
